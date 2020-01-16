97e2bf8b7ec8f0a449d7407f8273a32827dbc830
Enot <natasha.cat982404@gmail.com>
2020-01-16 19:04:48 +0200

    Dz-3
    Second task is done

--- js_dz3/dz3.js
+++ js_dz3/dz3.js
@@ -27,6 +27,7 @@ else{
 }
 shopList.forEach(x => document.write(`<p>${x.name}, ${x.amount}, ${x.isBought}</p>`));
 /*1.3*/
+document.write(`<h3>1.3</h3>`);
 function BuyGood(name){
     var changedGood = shopList.find(x => x.name == name);
     var index = shopList.indexOf(changedGood);
@@ -35,3 +36,34 @@ function BuyGood(name){
 }
 BuyGood("bread");
 shopList.forEach(x => document.write(`<p>${x.name}, ${x.amount}, ${x.isBought}</p>`));
+
+/*2.1*/
+document.write(`<h2>--2--</h2>`);
+document.write(`<h3>2.1</h3>`);
+var check = [
+    { name: "phone", count: 2, price: 2000 },
+    { name: "headphones", count: 1, price: 1200 },
+    { name: "lamp", count: 3, price: 500 },
+    { name: "notebook", count: 1, price: 5000 },
+    { name: "keyboard", count: 1, price: 5000 }
+]
+check.forEach(x => document.write(`<p>${x.name}, ${x.count}, ${x.price}$ : ${x.count * x.price}</p>`));
+
+/*2.2*/
+document.write(`<h3>2.2</h3>`);
+var fullPrice = 0;
+check.forEach(x => (fullPrice += x.count * x.price));
+document.write(`<p>FullPrice: ${fullPrice}$</p>`);
+
+/*2.3*/
+document.write(`<h3>2.3</h3>`);
+let maximum = Math.max.apply(Math, check.map(function(x) { return x.price; }));
+let maxXObjects = check.filter(function(x) { return x.price === maximum; });
+maxXObjects.forEach(x => document.write(`<p>${x.name}, ${x.count}, ${x.price}$ : ${x.price}</p>`));
+
+/*2.4*/
+document.write(`<h3>2.4</h3>`);
+var goodsCount = 0;
+check.forEach(x => (goodsCount += x.count));
+let avg = fullPrice / goodsCount;
+document.write(`<p>AVRG: ${avg}</p>`);
