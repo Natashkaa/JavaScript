4083b6e9625c9b22ee547b3a50da3d679095ad3a
Enot <natasha.cat982404@gmail.com>
2020-01-27 03:29:59 +0200

    Dz-8
    3 - work with element(s) selection (by keys) and change background

--- js_dz8/dz8.js
+++ js_dz8/dz8.js
@@ -12,10 +12,70 @@ elements.forEach( function(e) {
 });
 /*2*/
 var buttonLiArr = document.getElementsByClassName("opend");
-var i;
-
-for (i = 0; i < buttonLiArr.length; i++) {
+for (let i = 0; i < buttonLiArr.length; i++) {
     buttonLiArr[i].addEventListener("click", function() {
         this.parentElement.querySelector(".nested").classList.toggle("active");
   });
 }
+
+/*3*/
+let key;
+let ind1 = 0;
+let ind2 = 0;
+let selectedLi;
+document.addEventListener('keydown', function(event) {
+        key = event;
+});
+document.addEventListener('keyup', function(event) {
+    key = undefined;
+});
+
+var books = document.querySelectorAll('.book-list > li');
+book_list.onclick = function(event) {
+    let curLi = event.target.closest('li');
+    if(!curLi)  return;
+
+    if(!book_list.contains(curLi)) return;
+    
+    highlight(curLi);
+  };
+function highlight(curLi) {
+    if(key){
+        if(key.ctrlKey){
+            for (let i in books) {
+                if(books[i] == curLi){
+                    ind1 = i;
+                    break;
+                }
+            }
+            books[ind1].classList.toggle("orangeLi");  
+        }
+        else if(key.key == "Shift"){
+            for (let i in books) {
+                if(books[i] == curLi){
+                    ind2 = i;
+                    break;
+                }
+            }
+            if(ind1 > ind2){
+                for(ind2; ind2 <= ind1; ind2++){
+                    books[ind2].classList.add("orangeLi");
+                }
+            }
+            else{
+                for(ind2; ind2 >= ind1; ind2--){
+                    books[ind2].classList.add("orangeLi");
+                }
+            }
+        }
+    }
+    else{
+        books.forEach(x => x.classList.remove("orangeLi"));
+        curLi.classList.add("orangeLi");
+    }
+    key = undefined;
+}
+
+
+
+
