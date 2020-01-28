807df9716ab7a1c5cf38abda75469f0dc4ae4a6d
Enot <natasha.cat982404@gmail.com>
2020-01-28 23:59:02 +0200

    Dz-10

--- js_dz10/dz10.js
+++ js_dz10/dz10.js
@@ -2,15 +2,13 @@ let res;
 $('#incr_btn').on('click', () =>{
     res = parseInt($('#number').text()) + 1;
     $('#number').text(res);
-  });
-  
+  }); 
 $('#decr_btn').on('click', () =>{
     res = parseInt($('#number').text()) - 1;
     $('#number').text(res);
 });
 
 /*2*/
-
 $('#adddiv_btn').on('click', () =>{
     $('.divs').append($('<div />', { "class": 'el-d' })
                 .css({backgroundColor: `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`})
@@ -21,7 +19,11 @@ $('.el-d').click(function (){
     $(this).remove();	
 });
 
-
+/*3*/
+$('.item-color').click(function (){
+    var c = $(this).attr('data-color');
+    $('#some-text').css({color: `${c}`});
+});
 
 
 
